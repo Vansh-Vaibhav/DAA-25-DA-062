d65d74152f991b7913c53e83fe9b94c751836513
Vansh Vaibhav <v405ansh@gmail.com>
2026-09-13 16:18:52 +0530

    Activity Selection problem.cpp

--- Experiment7/Articulation Points.cpp
+++ Experiment7/Articulation Points.cpp
@@ -1,65 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int timer = 0;
-vector<int> tin, low;
-vector<bool> vis, ap;
+struct Activity {
+    int s, f;
+};
 
-void dfs(int u, int p, vector<vector<int>>& adj) {
-    vis[u] = true;
-    tin[u] = low[u] = ++timer;
-    int children = 0;
+int main() {
+    int n;
+    cin >> n;
 
-    for (int v : adj[u]) {
-        if (v == p)
-            continue;
+    vector<Activity> a(n);
 
-        if (vis[v]) {
-            low[u] = min(low[u], tin[v]);
-        } else {
-            dfs(v, u, adj);
-            low[u] = min(low[u], low[v]);
+    for (int i = 0; i < n; i++)
+        cin >> a[i].s >> a[i].f;
 
-            if (p != -1 && low[v] >= tin[u])
-                ap[u] = true;
+    sort(a.begin(), a.end(), [](Activity x, Activity y) {
+        return x.f < y.f;
+    });
 
-            children++;
+    cout << "Selected Activities:\n";
+
+    int last = -1;
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i].s >= last) {
+            cout << "(" << a[i].s << ", " << a[i].f << ")\n";
+            last = a[i].f;
+            count++;
         }
     }
 
-    if (p == -1 && children > 1)
-        ap[u] = true;
-}
-
-int main() {
-    int n, e;
-    cin >> n >> e;
-
-    vector<vector<int>> adj(n);
-
-    for (int i = 0; i < e; i++) {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-
-    tin.resize(n);
-    low.resize(n);
-    vis.assign(n, false);
-    ap.assign(n, false);
-
-    for (int i = 0; i < n; i++) {
-        if (!vis[i])
-            dfs(i, -1, adj);
-    }
-
-    cout << "Articulation Points: ";
-
-    for (int i = 0; i < n; i++) {
-        if (ap[i])
-            cout << i << " ";
-    }
-    cout<<"\nVansh Vaibhav CSDA 25/DA/062\n";
+    cout << "Maximum Activities: " << count << endl;
+    cout<<"Vansh Vaibhav\n";
     return 0;
 }
